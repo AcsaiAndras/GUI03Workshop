02ac1df8d9a2ddef4bf291407fa199aa0a105084
Budai Dávid <budaidavid72@gmail.com>
2023-03-29 11:07:37 +0200

    Progress bar color converter class added

---
+++ GUI03/PBColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace GUI03
+{
+    internal class PBColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int input = (int)value;
+            if (input < 4)
+            {
+                return Brushes.Red;
+            }
+            else if (input < 7)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.Green;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
